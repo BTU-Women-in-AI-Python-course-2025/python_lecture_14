3fc4277aa782c56d4c2a387c6a3de3fb181d54f6
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-17 19:51:46 +0400

    Add blog post list and blog post detail

--- Lecture code 1/blog_post/blog/urls.py
+++ Lecture code 1/blog_post/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from blog.views import create_blog_post, thank_you, create_blog_post_model_form, blog_detail
+from blog.views import not_found, blog_post_list, blog_post_detail
 
 urlpatterns = [
-    path('create_blog_post/', create_blog_post, name='create_blog_post'),
-    path('thank_you/', thank_you, name='thank_you'),
-    path('create_blog_post_model_form/', create_blog_post_model_form, name='create_blog_post_model_form'),
-    path('blog_detail/<int:post_id>/', blog_detail, name='blog_detail'),
+    path('not_found/', not_found, name='not_found'),
+    path('blog_post_list/', blog_post_list, name='blog_post_list'),
+    path('blog_post_detail/<int:pk>/', blog_post_detail, name='blog_post_detail'),
+
 ]
--- Lecture code 1/blog_post/blog/views.py
+++ Lecture code 1/blog_post/blog/views.py
@@ -1,41 +1,18 @@
-from django.db.models import Q
-from django.shortcuts import redirect, render, get_object_or_404
-from blog.forms import BlogPostForm, BlogPostModelForm
-from blog.models import BlogPost, BannerImage
+from django.shortcuts import render, redirect
+from blog.models import BlogPost
 
 
-def create_blog_post(request):
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST)
-        if form.is_valid():
-            BlogPost.objects.create(**form.cleaned_data)
-            return redirect('thank_you')
-    else:
-        form = BlogPostForm()
-    return render(request, template_name='create_blog_post.html', context={'form': form})
+def blog_post_list(request):
+    blog_posts = BlogPost.objects.all()
+    return render(request, template_name='blog_list.html', context={'blog_posts': blog_posts})
 
 
-def create_blog_post_model_form(request):
-    if request.method == 'POST':
-        form = BlogPostModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            blog_post = form.save()
-            banner_file = request.FILES.get('banner_image')
-            if banner_file:
-                BannerImage.objects.create(blog_post=blog_post, image=banner_file)
-            return redirect('thank_you')
-    else:
-        form = BlogPostModelForm()
-    return render(request, template_name='create_blog_post_model_form.html', context={'form': form})
+def blog_post_detail(request, pk):
+    blog = BlogPost.objects.filter(pk=pk).first()
+    if not blog:
+        return redirect('not_found')
+    return render(request, template_name='blog_detail.html', context={'blog': blog})
 
 
-def thank_you(request):
-    return render(request, template_name='thank_you.html')
-
-
-def blog_detail(request, post_id):
-    post = get_object_or_404(BlogPost, id=post_id)
-    return render(request, template_name='blog_detail.html', context={'post': post})
-
-
-# BlogPost.objects.filter(Q(deleted=True) | Q(active=True), id=post_id)
+def not_found(request):
+    return render(request, template_name='404.html')
