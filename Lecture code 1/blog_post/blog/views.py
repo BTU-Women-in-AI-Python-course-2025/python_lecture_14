5e1ce1a3a7b5ac578c5c995278ee99c1304ab46b
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-17 20:20:07 +0400

    Add blog_post_delete

--- Lecture code 1/blog_post/blog/views.py
+++ Lecture code 1/blog_post/blog/views.py
@@ -5,7 +5,7 @@ from blog.models import BlogPost, BannerImage
 
 
 def blog_post_list(request):
-    blog_posts = BlogPost.objects.all()
+    blog_posts = BlogPost.objects.filter(deleted=False)
     return render(request, template_name='blog_list.html', context={'blog_posts': blog_posts})
 
 
@@ -42,6 +42,20 @@ def blog_post_update(request, pk):
         form = UpdateBlogPostModelForm(instance=blog)
 
     return render(request, 'blog_update.html', {'form': form})
+
+def blog_post_delete(request, pk):
+    blog = BlogPost.objects.filter(pk=pk).first()
+    if not blog:
+        return redirect('not_found')
+
+    if request.method == 'POST':
+        BlogPost.objects.filter(id=pk).update(deleted=True)
+        # -- second way
+        # blog.deleted = True
+        # blog.save()
+        return redirect('blog_post_list')
+
+    return render(request, 'blog_post_confirm_delete.html', {'blog': blog})
 
 def not_found(request):
     return render(request, template_name='404.html')
