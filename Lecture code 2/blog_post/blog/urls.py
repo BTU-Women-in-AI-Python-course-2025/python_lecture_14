fefbfb9e2df8a49fecec66391ad2c97c8815d457
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-18 19:49:50 +0400

    Create list and detail views

--- Lecture code 2/blog_post/blog/urls.py
+++ Lecture code 2/blog_post/blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from blog.views import (
+    not_found,
+    blog_post_list,
+    blog_post_detail
+)
 
 urlpatterns = [
-
+    path('not_found', not_found, name='not_found'),
+    path('blog_list/', blog_post_list, name='blog_list'),
+    path('blog_detail/<int:pk>/', blog_post_detail, name='blog_detail'),
 ]
--- Lecture code 2/blog_post/blog/views.py
+++ Lecture code 2/blog_post/blog/views.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render, redirect
+from blog.models import BlogPost
 
+def not_found(request):
+    return render(request, '404.html')
+
+def blog_post_list(request):
+    blogs = BlogPost.objects.all()
+    return render(request, template_name='blog_list.html', context={'blogs': blogs})
+
+
+def blog_post_detail(request, pk):
+    blog = BlogPost.objects.filter(id=pk).first()
+    if not blog:
+        return redirect('not_found')
+    return render(request, template_name='blog_detail.html', context={'blog': blog})
